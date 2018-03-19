19d3381c1f84fd353348e892f9c9bf5823f4bb5b
JackSilence <jacksilence@gmail.com>
2018-03-19 23:58:22 +0800

    結合configserver先調整一版

--- src/main/java/com/ibm/demo/controller/DemoController.java
+++ src/main/java/com/ibm/demo/controller/DemoController.java
@@ -2,15 +2,26 @@ package com.ibm.demo.controller;
 
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.serviceregistry.Registration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class DemoController {
+	private final Logger log = LoggerFactory.getLogger( this.getClass() );
+
+	@Value( "${test.message:}" )
+	private String message;
+
 	@Autowired
 	private Registration registration;
 
@@ -19,6 +30,8 @@ public class DemoController {
 
 	@GetMapping( "/instance-info" )
 	public ServiceInstance showInfo() {
+		log.info( "Message: " + message + ", is empty: " + StringUtils.isEmpty( message ) );
+
 		// DiscoveryClient的getLocalServiceInstance方法已被標註為@Deprecated, 建議不要使用
 		List<ServiceInstance> list = discoveryClient.getInstances( registration.getServiceId() );
 
